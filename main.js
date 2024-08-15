6383a24189990fb287d97a81ad2eff9ba960d2c5
Andrew Local <andrew.buist99@outlook.com>
2024-08-15 19:45:36 +0100

    object add/clone pipeline streamlined

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ import { OrbitControls } from './scripts/three/examples/jsm/controls/OrbitContro
 import { GLTFLoader } from './scripts/three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from './scripts/three/examples/jsm/loaders/DRACOLoader.js'
 import { RGBELoader } from './scripts/three/examples/jsm/loaders/RGBELoader.js'
+import * as SkeletonUtils from './scripts/three/examples/jsm/utils/SkeletonUtils.js'
 
 //KEY VARIABLES//
 //Document
 var uiElement = document.querySelector('#loadingscreen')
 var uiLoadingBar = document.querySelector('#loadingbar')
-const using_mobile = "ontouchstart" in document.documentElement
+const USING_MOBILE = "ontouchstart" in document.documentElement
 
 //Scenes
-var active_scene = new THREE.Scene()
+var activeScene = new THREE.Scene()
 var scene1 = new THREE.Scene()
 
 new RGBELoader()
@@ -24,31 +25,24 @@ new RGBELoader()
     })
 
 //Models
-let base_mesh,
-    transparent_mesh,
-    interactive_mesh1,
-    interactive_mesh2,
-    interactive_mesh3,
-    interactive_mesh4,
-    blockstack,
-    businessman,
-    arrow
-
-//Lights
-var focus_light_intensity = 400
-var focus_light_colour = 0xfff5b6
-let focus_light
-
-//Empties
-let focus_target
+var models = {}
+var clips = {}
 
 //Animation mixer array
-var mixer_arr = []
+var mixerArr = []
+
+//Lights
+var focusLightInt = 400
+var focusLightCol = 0xfff5b6
+let focusLight
+
+//Empties
+let focusTarget
 
 //Canvas
 var myCanvas = document.getElementById('myCanvas')
-var height = window.innerHeight
-var width = window.innerWidth
+var windowHeight = window.innerHeight
+var windowWidth = window.innerWidth
 
 //Clock 
 var clock = new THREE.Clock()
@@ -62,7 +56,7 @@ var renderer = new THREE.WebGLRenderer({
 
 //renderer settings
 renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setSize(windowWidth, windowHeight)
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.gammaInput = true
@@ -71,33 +65,42 @@ renderer.antialias = true
 renderer.toneMapping = THREE.LinearToneMapping
 
 //Camera
-var distance = 2500
-var diag = Math.sqrt((height * height) + (width * width))
-var fov = 2 * Math.atan((diag) / (2 * distance)) * (180 / Math.PI) //Field of View
-var camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, distance)
-camera.position.set(0, 5, 0)
+var distance = 1750
+var orbitDistance = 1
+var fov = 50 //Field of View
+var camera = new THREE.PerspectiveCamera(fov, windowWidth / windowHeight, 0.01, distance)
+camera.position.set(orbitDistance*Math.sqrt(2)/2, 5, orbitDistance*Math.sqrt(2)/2)
 camera.zoom = 1
 camera.updateProjectionMatrix()
 
-//OrbitControls
-var orbit = new OrbitControls(camera, renderer.domElement)
-orbit.minPolarAngle = Math.PI / 3
-orbit.maxPolarAngle = Math.PI / 1.5
-orbit.maxDistance = 6
-orbit.target = new THREE.Vector3(-1, 5, 0)
-orbit.reversed = false
-orbit.enablePan = false
-orbit.update()
+//controlsControls
+var controls = new OrbitControls(camera, renderer.domElement)
+controls.target.set(0,5,0)
+controls.enableZoom = false
+controls.enablePan = false
+
+function retargetControls(target, distance) {
+    controls.target.copy(new THREE.Vector3().fromArray(target))
+
+    const pos = new THREE.Vector3().fromArray(target)
+    const dist = camera.position.distanceTo(pos)
+    // if distance is less than minimum distance, set the camera position to minimum distance
+    if (dist > distance) {
+        const dir = camera.position.clone().sub(pos).normalize()
+        const align = pos.clone().add(dir.multiplyScalar(distance))
+        camera.position.set(align.x, align.y, align.z)
+        }
+}
 
 //Raycasting
-var cursor_delta = 6
-let startX
-let startY
+var cursorDelta = 6
 var raycaster = new THREE.Raycaster()
 var pointer = new THREE.Vector2()
+let startX
+let startY
 let intersects
-let target_intersect
-let target_name
+let targetIntersect
+let targetName
 
 //Painting Links
 var links = {
@@ -109,11 +112,7 @@ var links = {
 }
 
 //Locations
-var locs = {
-    "a1": [0, 5, 0],
-    "a2": [0, 5, 10],
-    "a3": [0, 5, -10]
-}
+var locs = {}
 
 // Instantiate a loading manager
 var manager = new THREE.LoadingManager()
@@ -124,14 +123,14 @@ manager.onStart = function () {
 }
 
 manager.onProgress = function (url, itemsLoaded, itemsTotal) {
-    console.log(url)
-    var load_percent = ((itemsLoaded / itemsTotal) * 100).toFixed(2)
+    //console.log(url)
+    const load_percent = ((itemsLoaded / itemsTotal) * 100).toFixed(2)
     uiLoadingBar.style.width = load_percent + "%"
 }
 
 manager.onLoad = function () {
     //Remove loading screen background
-    active_scene = scene1
+    activeScene = scene1
     uiElement.style.display = 'none'
     document.body.appendChild(renderer.domElement)
 }
@@ -146,96 +145,118 @@ loader.setDRACOLoader(dracoLoader)
 
 ////
 
-function addScene(gltf, rename, transparent = false, animated = false, position = [0, 0, 0], rotation = [0, 0, 0], scale = [1, 1, 1]) {
+//Generic function to zip 2 arrays to named key:value obj
+function zipArrays(keys, values) {
+    const result = {}
+    for (let i = 0; i < keys.length; i++) {
+        result[keys[i]] = values[i]
+    }
+    return result
+}
+
+//Creates a blob of all 3d items under total promise, then assigns to named global lists
+async function loadPush(gltfArr, nameArr) {
+    const loadBlob = await Promise.all(gltfArr.map(x => loader.loadAsync(x)))
+
+    models = zipArrays(nameArr, loadBlob.map(x => x.scene))
+    clips = zipArrays(nameArr, loadBlob.map(x => x.animations))
+}
+
+function addToScene(sceneName, par = {}) {
+    if (!par.position) { par.position = [0, 0, 0] }
+    if (!par.rotation) { par.rotation = [0, 0, 0] }
+    if (!par.scale) { par.scale = [1, 1, 1] }
+    if (!par.rename) { par.rename = sceneName }
+
+    const obj = SkeletonUtils.clone(models[sceneName])
+
     //transform block//
-    gltf.scene.position.x = position[0]
-    gltf.scene.position.y = position[1]
-    gltf.scene.position.z = position[2]
-
-    gltf.scene.scale.x = scale[0]
-    gltf.scene.scale.y = scale[1]
-    gltf.scene.scale.z = scale[2]
-
-    gltf.scene.rotation.x = rotation[0]
-    gltf.scene.rotation.y = rotation[1]
-    gltf.scene.rotation.z = rotation[2]
+    obj.position.copy(new THREE.Vector3().fromArray(par.position))
+    obj.rotation.copy(new THREE.Euler().fromArray(par.rotation))
+    obj.scale.copy(new THREE.Vector3().fromArray(par.scale))
     ////
 
-    //apply animations//
-    if (animated) {
-        var mixer = new THREE.AnimationMixer(gltf.scene)
-        gltf.animations.forEach((clip) => {
-
-            mixer.clipAction(clip).play()
-
-        })
-        mixer_arr.push(mixer)
+    if(par.nav){
+        locs[par.rename] = par.position.map( function(n,i){return n + par.nav[i]}) //[par.position[0] + par.nav[0], par.position[1] + par.nav[1], par.position[2] + par.nav[2]]
     }
+
+    //apply animations//
+    //note: all instances will have an associated mixer in the mixerArr, even if no anims present//
+    var mixer = new THREE.AnimationMixer(obj)
+    clips[sceneName].forEach((clip) => {
+
+        mixer.clipAction(clip).play()
+
+    })
+    mixerArr.push(mixer)
     ////
 
     //material overrides for transparency, envIntensity, and rename//
-    gltf.scene.traverse(function (child) {
+    obj.traverse(function (child) {
         if (child instanceof THREE.Mesh) {
-            //child.material.alphaHash = true
-            if (transparent) {
+            //assign name to each element of mesh for raycast name passback
+            child.name = par.rename
+            child.frustumCulled = false
+
+            if (child.material.name.includes("trans")) {
                 child.material.trasparent = true
             }
-            if (rename) {
-                child.name = rename
-            }
-
             child.material.envMapIntensity = 0.3
         }
     })
     ////
 
-    scene1.add(gltf.scene)
+    scene1.add(obj)
 }
 
 //Scene1 (main) .adds
 async function init() {
-    //asset loader to push an array on promise
-    [
-        base_mesh,
-        transparent_mesh,
-        interactive_mesh1,
-        interactive_mesh2,
-        interactive_mesh3,
-        interactive_mesh4,
-        blockstack,
-        businessman,
-        arrow
-    ] = await Promise.all([
-        loader.loadAsync("./3d_scenery/museum_hall.glb"),
-        loader.loadAsync("./3d_scenery/museum_hall_plants_alpha.glb"),
-        loader.loadAsync("./3d_scenery/museum_hall_painting1.glb"),
-        loader.loadAsync("./3d_scenery/museum_hall_painting2.glb"),
-        loader.loadAsync("./3d_scenery/museum_hall_painting3.glb"),
-        loader.loadAsync("./3d_scenery/museum_hall_painting4.glb"),
-        loader.loadAsync("./3d_scenery/blockstack.glb"),
-        loader.loadAsync("./3d_scenery/businessman.glb"),
-        loader.loadAsync("./3d_scenery/arrow.glb")
-    ])
+    await loadPush(
+        [
+            "./3d_scenery/museum_hall.glb",
+            "./3d_scenery/museum_hall_plants_alpha.glb",
+            "./3d_scenery/museum_hall_painting1.glb",
+            "./3d_scenery/museum_hall_painting2.glb",
+            "./3d_scenery/museum_hall_painting3.glb",
+            "./3d_scenery/museum_hall_painting4.glb",
+            "./3d_scenery/blockstack.glb",
+            "./3d_scenery/businessman.glb",
+            "./3d_scenery/arrow.glb"
+        ],
+        [
+            "building",
+            "plants",
+            "link1",
+            "link2",
+            "link3",
+            "link4",
+            "link5",
+            "businessman",
+            "arrow"
+        ])
 
-    addScene(base_mesh, "building")
-    addScene(transparent_mesh, "plants", true)
-    addScene(interactive_mesh1, "link1")
-    addScene(interactive_mesh2, "link2")
-    addScene(interactive_mesh3, "link3")
-    addScene(interactive_mesh4, "link4")
-    addScene(blockstack, "link5")
-    addScene(businessman, "businessman", false, true, [6.4203, -0.8, -3.1523], [0, -Math.PI / 2, 0], [3, 3, 3])
-    //addScene(arrow, "a1", false, true, [0, 0, 10])
+    addToScene("building")
+    addToScene("plants")
+    addToScene("link1")
+    addToScene("link2")
+    addToScene("link3")
+    addToScene("link4")
+    addToScene("link5")
+    addToScene("businessman", { position: [6.4203, -0.8, -3.1523], rotation: [0, -Math.PI / 2, 0], scale: [3, 3, 3] })
+    addToScene("arrow", { position: [0, 0, 0], rename: "a1", nav: [0,5,0]})
+    addToScene("arrow", { position: [0, 0, 10], rename: "a2", nav: [0,5,0] })
+    addToScene("arrow", { position: [0, 0, -10], rename: "a3", nav: [0,5,0] })
+    addToScene("arrow", { position: [6.4203, 2, -1], rename: "a4", nav: [0,3,0] })
 
     //Lights and fog
-    focus_light = new THREE.SpotLight(focus_light_colour, focus_light_intensity, 0, Math.PI / 6, .3)
-    focus_light.castShadow = true
-    focus_target = new THREE.Object3D()
-    focus_target.position.set(0, 20, 0)
-    scene1.add(focus_target)
-    focus_light.target = focus_target
-    focus_light.position.set(0, 10, 0)
-    scene1.add(focus_light)
+    focusLight = new THREE.SpotLight(focusLightCol, focusLightInt, 0, Math.PI / 10, .3)
+    focusLight.castShadow = true
+    focusTarget = new THREE.Object3D()
+    focusTarget.position.set(0, 20, 0)
+    scene1.add(focusTarget)
+    focusLight.target = focusTarget
+    focusLight.position.set(0, 10, 0)
+    scene1.add(focusLight)
 
     scene1.fog = new THREE.Fog(0xffffff, 15, 50)
 }
@@ -244,25 +265,17 @@ async function init() {
 
 function render() {
     //constrain movement to bbox
-    //orbit.target.clamp(new THREE.Vector3(-1.5, 5, -6), new THREE.Vector3(1.5, 5, 6))
+    //controls.target.clamp(new THREE.Vector3(-1.5, 5, -6), new THREE.Vector3(1.5, 5, 6))
     //exposure
-    renderer.render(active_scene, camera)
-
-    requestAnimationFrame(render)
-}
-
-function animate() {
-    requestAnimationFrame(animate)
-
     var delta = clock.getDelta()
-
     //iterate through array and update to delta time
-    if (mixer_arr.length > 0) {
-        mixer_arr.forEach((element) => element.update(delta))
+    if (mixerArr.length > 0) {
+        mixerArr.forEach((element) => element.update(delta))
     }
 
-    renderer.render(scene1, camera)
-
+    controls.update(delta)
+    renderer.render(activeScene, camera)
+    requestAnimationFrame(render)
 }
 
 // Window sizing
@@ -276,7 +289,7 @@ function onWindowResize() {
 
 window.addEventListener('pointermove', function (event) {
 
-    if (using_mobile) {
+    if (USING_MOBILE) {
         pointer.x = 0
         pointer.y = 0
     } else {
@@ -284,27 +297,25 @@ window.addEventListener('pointermove', function (event) {
         pointer.y = - (event.clientY / window.innerHeight) * 2 + 1
     }
     raycaster.setFromCamera(pointer, camera)
-    intersects = raycaster.intersectObjects(active_scene.children)
+    intersects = raycaster.intersectObjects(activeScene.children)
     if (intersects.length > 0) {
-        target_intersect = intersects.reduce((prev, curr) => prev.distance < curr.distance ? prev : curr)
+        targetIntersect = intersects.reduce((prev, curr) => prev.distance < curr.distance ? prev : curr)
 
         //multiple materials on one mesh are given the name "xxx", "xxx_1", ...
-        target_name = target_intersect.object.name.split("_")[0]
-        //console.log(target_name)
+        targetName = targetIntersect.object.name.split("_")[0]
+        //console.log(targetName)
 
         //get the link for any objects which have them
-        if (Object.keys(links).indexOf(target_name) >= 0) {
+        if (Object.keys(links).indexOf(targetName) >= 0) {
             myCanvas.style.cursor = "pointer"
-            focus_light.intensity = focus_light_intensity
-            focus_light.position.z = target_intersect.point.z
-            focus_light.target.position.x = target_intersect.point.x
-            focus_light.target.position.y = target_intersect.point.y
-            focus_light.target.position.z = target_intersect.point.z
+            focusLight.intensity = focusLightInt
+            focusLight.position.z = targetIntersect.point.z
+            focusLight.target.position.copy(targetIntersect.point)
         } else {
             myCanvas.style.cursor = "default"
-            focus_light.intensity = 0
-            focus_light.position.z = 0
-            focus_light.position.y = 10
+            focusLight.intensity = 0
+            focusLight.position.z = 0
+            focusLight.position.y = 10
         }
     } else {
         //console.log("no hit!")
@@ -320,14 +331,18 @@ window.addEventListener('pointerup', function (event) {
     var diffX = Math.abs(event.clientX - startX)
     var diffY = Math.abs(event.clientY - startY)
 
-    if (diffX < cursor_delta && diffY < cursor_delta) {
+    if (diffX < cursorDelta && diffY < cursorDelta) {
         //redirect to links
-        var goto_address = links[target_name]
-        if (goto_address != undefined) {
-            gtag('event', 'link_out', {
-                'url': goto_address
-            })
-            window.location.href = goto_address
+        if (targetName in links) {
+            const gotoAddress = links[targetName]
+            if (gotoAddress != undefined) {
+                gtag('event', 'link_out', {
+                    'url': gotoAddress
+                })
+                window.location.href = gotoAddress
+            }
+        } else if (targetName in locs) {
+            retargetControls(locs[targetName], orbitDistance)
         }
     }
 })
@@ -336,5 +351,4 @@ window.addEventListener('pointerup', function (event) {
 {
     init()
     render()
-    animate()
 }
